805d3ff759de7d606a5bd2cb7b92c40f1b72055b
Janina Mass <janina.mass@gmail.com>
2022-03-06 09:16:13 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+
+fn main() {
+
+}
+
+
+#[test]
+fn test_imports_player(){
+    serpent::serpent::player::hello_player();
+}
+
+#[test]
+fn test_imports_obstacle(){
+    serpent::serpent::obstacle::hello_obstacle();
+}
