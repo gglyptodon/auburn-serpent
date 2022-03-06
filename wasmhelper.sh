eed15ac079cb079ce8ab7b54f470504fd9d55b7c
gglyptodon <janina.mass@gmail.com>
2022-03-06 14:37:10 +0100

    wasm output

--- wasmhelper.sh
+++ wasmhelper.sh
@@ -1,4 +1,4 @@
 cargo build --target wasm32-unknown-unknown --release
-wasm-bindgen target/wasm32-unknown-unknown/release/serpent.wasm --out-dir ./wasm_help --no-modules --no-typescript
-echo "serve via:   python3 -m http.server --directory wasm_help/"wasm-bindgen target/wasm32-unknown-unknown/release/main.wasm --out-dir ./wasm_help --no-modules --no-typescript
-wasm-bindgen target/wasm32-unknown-unknown/release/serpent.wasm --out-dir ./wasm_help --no-modules --no-typescript
+wasm-bindgen target/wasm32-unknown-unknown/release/main.wasm --out-dir ./wasm_help --no-modules --no-typescript
+echo "serve via:   python3 -m http.server --directory wasm_help/"
+#wasm-bindgen target/wasm32-unknown-unknown/release/main.wasm --out-dir ./wasm_help --no-modules --no-typescript
