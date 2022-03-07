fa9b71c95a21d21e3b1ea26eed8976bd26435918
gglyptodon <janina.mass@gmail.com>
2022-03-07 16:35:31 +0100

    resize

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ fn main() -> BResult<()> {
         .with_font("tiles4.png", 32, 32)
         .with_simple_console(serpent::WIDTH, serpent::HEIGHT, "tiles4.png")
         .with_fancy_console(serpent::WIDTH, serpent::HEIGHT, "tiles4.png")
-        .with_title("")
+        .with_title("Serpent")
         .with_fps_cap(30.0)
-        .with_tile_dimensions(32, 32)
+        //.with_tile_dimensions(16, 16)
         .build()?;
 
     main_loop(context, misc::State::new())
